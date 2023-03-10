4e35caaa403873f3f7f6393b6e0673c84c019b69
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-03-10 15:22:05 +0545

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,13 +5,13 @@ apt update
 apt install -y apache2 php
 
 # Enable PHP module in Apache2
-sed -i 's/#LoadModule php_module/LoadModule php_module/g' /etc/apache2/httpd.conf
+sed -i 's/#LoadModule php_module/LoadModule php_module/g' /data/data/com.termux/files/usr/etc/apache2/httpd.conf
 
 # Change DocumentRoot to /storage/emulated/0/server
-sed -i 's|DocumentRoot /data/data/com.termux/files/home|DocumentRoot /storage/emulated/0/server|g' /etc/apache2/httpd.conf
+sed -i 's|DocumentRoot /data/data/com.termux/files/usr/share/apache2/default-site/htdocs|DocumentRoot /storage/emulated/0/server|g' /data/data/com.termux/files/usr/etc/apache2/httpd.conf
 
 # Change Directory Index to index.php and index.html
-sed -i 's|DirectoryIndex index.html|DirectoryIndex index.php index.html|g' /etc/apache2/httpd.conf
+sed -i 's|DirectoryIndex index.html|DirectoryIndex index.php index.html|g' /data/data/com.termux/files/usr/etc/apache2/httpd.conf
 
 
 # Start Apache2
