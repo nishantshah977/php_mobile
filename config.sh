6c659866b981067780ffaa1ad0a40af3ab42cfe3
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-03-10 16:50:34 +0545

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,7 +11,7 @@ echo "ServerName localhost" >> $PREFIX/etc/apache2/httpd.conf
 echo "LoadModule php_module /data/data/com.termux/files/usr/libexec/apache2/libphp.so" >> $PREFIX/etc/apache2/httpd.conf
 echo "<FilesMatch \.php$>
   SetHandler application/x-httpd-php
-</FilesMatch>"
+</FilesMatch>" >> $PREFIX/etc/apache2/httpd.conf
 
 # Change DocumentRoot to /storage/emulated/0/server
 sed -i 's|DocumentRoot "/data/data/com.termux/files/usr/share/apache2/default-site/htdocs"|DocumentRoot "/storage/emulated/0/server"|g' $PREFIX/etc/apache2/httpd.conf
