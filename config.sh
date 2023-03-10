4c7a25e715f8a9ed5df8ecb7d587c1bbb9ff6acc
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-03-10 23:31:34 +0545

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+termux-setup-storage
+
 # Install Apache2 and PHP
 pkg update
 pkg upgrade
 
-termux-setup-storage
-cd /storage/emulated/0/
-mkdir server
-chmod 755 server
-cd $HOME
+
 
 pkg install apache2
 pkg install php-apache
 
+cd /storage/emulated/0/
+mkdir server
+chmod 755 server
+cd $HOME
 
 
 # Enable PHP module in Apache2
@@ -23,14 +25,15 @@ echo "<FilesMatch \.php$>
 </FilesMatch>" >> $PREFIX/etc/apache2/httpd.conf
 
 # Change DocumentRoot to /storage/emulated/0/server
-sed -i 's|DocumentRoot "/data/data/com.termux/files/usr/share/apache2/default-site/htdocs"|DocumentRoot "/storage/emulated/0/server"|g' $PREFIX/etc/apache2/httpd.conf
+sed -i 's|DocumentRoot "/data/data/com.termux/files/usr/share/apache2/default-site/htdocs"|DocumentRoot "/storage/emulated/0/server"|g' >> $PREFIX/etc/apache2/httpd.conf
 
+sed -i 's|<Directory "/data/data/com.termux/files/usr/share/apache2/default-site/htdocs">|<Directory "/storage/emulated/0/server"|g'  >> $PREFIX/etc/apache2/httpd.conf
 # Change Directory Index to index.html index.php
 sed -i 's|DirectoryIndex index.html|DirectoryIndex index.html index.php|g' $PREFIX/etc/apache2/httpd.conf
 
-sed -i 's|LoadModule mpm_worker_module libexec/apache2/mod_mpm_worker.so|#LoadModule mpm_worker_module libexec/apache2/mod_mpm_worker.so|g' $PREFIX/etc/apache2/httpd.conf
+sed -i 's|LoadModule mpm_worker_module libexec/apache2/mod_mpm_worker.so|#LoadModule mpm_worker_module libexec/apache2/mod_mpm_worker.so|g' >> $PREFIX/etc/apache2/httpd.conf
 
-sed -i 's|#LoadModule mpm_prefork_module libexec/apache2/mod_mpm_prefork.so|LoadModule mpm_prefork_module libexec/apache2/mod_mpm_prefork.so|g' $PREFIX/etc/apache2/httpd.conf
+sed -i 's|#LoadModule mpm_prefork_module libexec/apache2/mod_mpm_prefork.so|LoadModule mpm_prefork_module libexec/apache2/mod_mpm_prefork.so|g' >> $PREFIX/etc/apache2/httpd.conf
 
 
 # Start Apache2
