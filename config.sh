c3bc522c6711da8ec4b67731b0b45f849b12ccd5
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-03-10 15:29:09 +0545

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # Install Apache2 and PHP
-apt update
-apt install -y apache2 php
+pkg update
+pkg install -y apache2 php
 
 # Enable PHP module in Apache2
-sed -i 's/#LoadModule php_module/LoadModule php_module/g' /data/data/com.termux/files/usr/etc/apache2/httpd.conf
+echo "LoadModule php7_module libexec/apache2/libphp7.so" >> $PREFIX/etc/apache2/httpd.conf
+
+# Open Apache2 configuration file in Nano editor
+nano $PREFIX/etc/apache2/httpd.conf
 
 # Change DocumentRoot to /storage/emulated/0/server
-sed -i 's|DocumentRoot /data/data/com.termux/files/usr/share/apache2/default-site/htdocs|DocumentRoot /storage/emulated/0/server|g' /data/data/com.termux/files/usr/etc/apache2/httpd.conf
+sed -i 's|DocumentRoot "/data/data/com.termux/files/home"|DocumentRoot "/storage/emulated/0/server"|g' $PREFIX/etc/apache2/httpd.conf
 
-# Change Directory Index to index.php and index.html
-sed -i 's|DirectoryIndex index.html|DirectoryIndex index.php index.html|g' /data/data/com.termux/files/usr/etc/apache2/httpd.conf
-
+# Change Directory Index to index.html index.php
+sed -i 's|DirectoryIndex index.html|DirectoryIndex index.html index.php|g' $PREFIX/etc/apache2/httpd.conf
 
 # Start Apache2
 apachectl start
 
 echo "Apache2 and PHP have been installed and configured successfully."
-echo "You can access the server at http://localhost:8080/"
